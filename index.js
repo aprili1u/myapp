ce2c56a6e1cd1a15e8c208608dd347da702f865e
Aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2019-11-20 15:27:48 +0100

    Message

---
+++ index.js
@@ -0,0 +1,25 @@
+//Import library
+
+const express = require("express");
+
+//use default PORT or given by platform.
+const PORT = process.env.PORT || 80;
+
+//Create application
+const app = express();
+
+//some basic route
+app.get("/", (req, res) => {
+  //send response.
+  res.json({ message: "coucou" });
+});
+
+//Start server.
+
+app.listen(80, err => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.info("Ser started.");
+});
